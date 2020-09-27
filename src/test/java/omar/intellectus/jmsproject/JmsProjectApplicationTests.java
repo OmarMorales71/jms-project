e75fe6bfcf2239cd1f69337004768f007041e254
Omar Morales <morales.ortega.omar@gmail.com>
2020-09-27 17:58:05 -0500

    first commit

---
+++ src/test/java/omar/intellectus/jmsproject/JmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package omar.intellectus.jmsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
